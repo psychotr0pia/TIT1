ca8064ecefc2bd0ac56d38e8a8a7fd02eab58642
Alejandro Villalobos <101144664+psychotr0pia@users.noreply.github.com>
2023-09-27 21:54:15 -0300

    Delete backEnd/backend directory

--- backEnd/backend/routes/main_route.js
+++
@@ -1,8 +0,0 @@
-const { response } = require("express");
-
-module.exports = function(app, config){
-
-    app.get('/',(request, response) => {
-        response.json({"mensaje": "Hola mundo"});
-    });
-}
